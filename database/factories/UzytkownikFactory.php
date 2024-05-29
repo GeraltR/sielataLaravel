aad4ea291c1e49fdf1e75e3840dc877740fc33cf
GeraltR <donchichot@sielata.com.pl>
2024-05-29 18:49:36 +0200

    Begin Laravel Breeze, Lang and Sanctum
    Oryginal files

---
+++ database/factories/UzytkownikFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UzytkownikFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function unverified()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'email_verified_at' => null,
+            ];
+        });
+    }
+}
